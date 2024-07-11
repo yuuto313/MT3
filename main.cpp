1329d1ae30e4ccfef2babac26a6e0e468ed5f0a2
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-07-11 21:56:40 +0900

    03_02課題

--- main.cpp
+++ main.cpp
@@ -1,58 +1,16 @@
 #include <Novice.h>
-#include <MyMath.h>
+#include "MyMath.h"
 
 const char kWindowTitle[] = "LE2B_04_オザワ_ユウト";
+
+Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) { return Multiply(m1, m2); }
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	//肘と手のwordMatrixを求める
-	//資料6~8辺り
-
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 	
-	//各配列の値は[0]:肩,[1]:肘,[2]:手を表す
-
-	Vector3 translates[3] = {
-		{0.2f,1.0f,0.0f},
-		{0.4f,0.0f,0.0f},
-		{0.3f,0.0f,0.0f}
-	};
-	Vector3 rotates[3] = {
-		{0.0f,0.0f,-6.8f},
-		{0.0f,0.0f,-1.4f},
-		{0.0f,0.0f,0.0f}
-	};
-	Vector3 scales[3] = {
-		{1.0f,1.0f,1.0f},
-		{1.0f,1.0f,1.0f},
-		{1.0f,1.0f,1.0f}
-	};
-
-
-	Sphere sphere[3] = {
-		{translates[0],0.05f},
-		{translates[1],0.05f},
-		{translates[2],0.05f}
-	};
-
-	Vector3 rotate = {};
-	Vector3 translate = {};
-
-	Matrix4x4 rotateMatrix = {};
-	Matrix4x4 rotateOBBMatrix = {};
-	Matrix4x4 obbWorldMatrix = {};
-
-	//int color = BLUE;
-
-	int kWindowWidth = 1280;
-	int kWindowHeight = 720;
-
-	//おすすめ初期数値
-	Vector3 cameraTranslate = { 0.0f,1.9f,-6.49f };
-	Vector3 cameraRotate = { 0.26f,0.0f,0.0f };
-
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -69,48 +27,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		
-		//肩のworldMatrix
-		Matrix4x4 shoulderWorldMatrix = MakeAffineMatrix(scales[0], rotates[0], translates[0]);
-		//肘のworldMatrix
-		Matrix4x4 elbowLocalMatrix = MakeAffineMatrix(scales[1], rotates[1], translates[1]);
-		Matrix4x4 elbowWorldMatrix = Multiply(elbowLocalMatrix, shoulderWorldMatrix);
-		//手のworldMatrix
-		Matrix4x4 handLocalMatrix = MakeAffineMatrix(scales[2], rotates[2], translates[2]);
-		Matrix4x4 handWorldMatrix = Multiply(handLocalMatrix, elbowWorldMatrix);
 
-		sphere[0].center = Transform({ 0.0f, 0.0f, 0.0f }, shoulderWorldMatrix);
-		sphere[1].center = Transform({ 0.0f, 0.0f, 0.0f }, elbowWorldMatrix);
-		sphere[2].center = Transform({ 0.0f, 0.0f, 0.0f }, handWorldMatrix);
-
-		//各種行列の計算
-		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		//WVPMatrixを作る
-		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		//ViewportMatrixを作る
-		Matrix4x4 viewPortMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-
-		//OBBの軸を作る
-		rotateMatrix = Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z))); 
-
+		Vector3 a{ 0.2f,1.0f,0.0f };
+		Vector3 b{ 2.4f,3.1f,1.2f };
+		Vector3 c = a + b;
+		Vector3 d = a - b;
+		Vector3 e = a * 2.4f;
+		Vector3 rotate{ 0.4f,1.43f,-0.8f };
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+		Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
 
 		ImGui::Begin("Window");
-		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
-		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
-
-		ImGui::SliderFloat3("translates[0]", &translates[0].x, -3.f, 3.f);
-		ImGui::SliderFloat3("translates[1]", &translates[1].x, -3.f, 3.f);
-		ImGui::SliderFloat3("translates[2]", &translates[2].x, -3.f, 3.f);
-		ImGui::SliderFloat3("scales[0]", &scales[0].x, -3.f, 3.f);
-		ImGui::SliderFloat3("scales[1]", &scales[1].x, -3.f, 3.f);
-		ImGui::SliderFloat3("scales[2]", &scales[2].x, -3.f, 3.f);
-		ImGui::SliderFloat3("rotates[0]", &rotates[0].x, -3.f, 3.f);
-		ImGui::SliderFloat3("rotates[1]", &rotates[1].x, -3.f, 3.f);
-		ImGui::SliderFloat3("rotates[2]", &rotates[2].x, -3.f, 3.f);
-
+		
+		ImGui::Text("c:%f,%f,%f", c.x, c.y, c.z);
+		ImGui::Text("d:%f,%f,%f", d.x, d.y, d.z);
+		ImGui::Text("e:%f,%f,%f", e.x, e.y, e.z);
+		ImGui::Text("matrix:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
+			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
+			rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
+			rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]
+			);
 	
 		ImGui::End();
 
@@ -123,14 +62,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		DrawGrid(worldViewProjectionMatrix, viewPortMatrix);
-		DrawSphere(sphere[0], worldViewProjectionMatrix, viewPortMatrix,RED);
-		DrawSphere(sphere[1], worldViewProjectionMatrix, viewPortMatrix,GREEN);
-		DrawSphere(sphere[2], worldViewProjectionMatrix, viewPortMatrix,BLUE);
-		//肩から肘への線
-		DrawLine(sphere[0].center, sphere[1].center, worldViewProjectionMatrix, viewPortMatrix, WHITE);
-		//肘から手への線
-		DrawLine(sphere[1].center, sphere[2].center, worldViewProjectionMatrix, viewPortMatrix, WHITE);
+		
 		
 
 
