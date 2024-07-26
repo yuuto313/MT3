f499e550e2caa6b7515aa5aa1cd91ad4a8fe4656
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-07-26 12:22:06 +0900

    修正

--- MyMath.h
+++ MyMath.h
@@ -191,3 +191,13 @@ bool IsCollisionSegmentAndAABB(const AABB& aabb, const Segment& segment);
 
 //球とOBBの当たり判定
 bool IsCollisionSphereAndOBB(const Sphere& sphere, const OBB& obb);
+
+
+//二項演算子
+Vector3 operator+(const Vector3& v1, const Vector3& v2);
+Vector3 operator-(const Vector3& v1, const Vector3& v2);
+
+Vector3 operator+(float s, const Vector3& v);
+Vector3 operator*(float s, const Vector3& v);
+Vector3 operator*(const Vector3& v, float s);
+Vector3 operator/(const Vector3& v, float s);
